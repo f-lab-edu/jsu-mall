11737e3ebc2def2e3e1e3d812b7c54a44c1ddb54
감자 <wjdwjdtn92@naver.com>
2024-05-04 18:08:54 +0900

    Feat: Apply changed lint rule

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -4,6 +4,8 @@ import { signInWithGoogle } from '@/serivce/supabase/google';
 
 export default function LoginPage() {
   return (
-    <button onClick={() => signInWithGoogle({ next: '/' })}>구글 로그인</button>
+    <button type="button" onClick={() => signInWithGoogle({ next: '/' })}>
+      구글 로그인
+    </button>
   );
 }
