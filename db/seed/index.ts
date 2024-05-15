25935acb4910117a1a6f19ff448a4907d420f2de
감자 <wjdwjdtn92@naver.com>
2024-05-15 12:28:00 +0900

    Feat: Add delete all data in seed

--- db/seed/index.ts
+++ db/seed/index.ts
@@ -9,6 +9,7 @@ import {
   product_size,
   size
 } from 'db/schema';
+import { sql } from 'drizzle-orm';
 
 import BRAND from './BRAND.json';
 import CATEGORY from './CATEOGORY.json';
@@ -23,61 +24,122 @@ function rand(min: number, max: number) {
 }
 
 (async () => {
-  Promise.all([
-    db.insert(category).values(CATEGORY),
-    db.insert(color).values(COLOR),
-    db.insert(brand).values(BRAND),
-    db.insert(size).values(SIZE)
-  ]);
+  try {
+    await Promise.all([
+      db.delete(product_delivery),
+      db.delete(product_quantity),
 
-  const products = await db.insert(product).values(PRODUCT).returning();
-
-  for (const product of products) {
-    Promise.all([
-      db.insert(product_color).values([
-        {
-          product_id: product.id,
-          color_id: rand(1, 3)
-        },
-        {
-          product_id: product.id,
-          color_id: rand(4, 6)
-        },
-        {
-          product_id: product.id,
-          color_id: rand(7, 9)
-        }
-      ]),
-
-      db.insert(product_size).values([
-        {
-          product_id: product.id,
-          size_id: 1
-        },
-        {
-          product_id: product.id,
-          size_id: 2
-        },
-        {
-          product_id: product.id,
-          size_id: 3
-        }
-      ]),
-
-      db.insert(product_quantity).values([
-        {
-          product_id: product.id,
-          quantity: rand(1, 100)
-        }
-      ]),
-
-      db.insert(product_delivery).values({
-        product_id: product.id,
-        shipping_fee: 1000,
-        duration_days: rand(1, 5)
-      })
+      db.delete(product_size),
+      db.delete(product_color)
     ]);
-  }
 
-  console.log('Seed data inserted');
+    await db.delete(product);
+
+    await Promise.all([
+      db.delete(size),
+      db.delete(color),
+      db.delete(brand),
+      db.delete(category)
+    ]);
+
+    await Promise.all([
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('cart', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('product', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('order', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('order_item', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('category', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('brand', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('product_delivery', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('color', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('product_quantity', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('size', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('product_color', 'id'), 1, false)`
+      ),
+      db.execute(
+        sql`SELECT setval(pg_get_serial_sequence('product_size', 'id'), 1, false)`
+      )
+    ]);
+
+    await Promise.all([
+      db.insert(category).values(CATEGORY),
+      db.insert(color).values(COLOR),
+      db.insert(brand).values(BRAND),
+      db.insert(size).values(SIZE)
+    ]);
+
+    const products = await db.insert(product).values(PRODUCT).returning();
+
+    for (const product of products) {
+      await Promise.all([
+        db.insert(product_color).values([
+          {
+            product_id: product.id,
+            color_id: rand(1, 3)
+          },
+          {
+            product_id: product.id,
+            color_id: rand(4, 6)
+          },
+          {
+            product_id: product.id,
+            color_id: rand(7, 9)
+          }
+        ]),
+
+        db.insert(product_size).values([
+          {
+            product_id: product.id,
+            size_id: 1
+          },
+          {
+            product_id: product.id,
+            size_id: 2
+          },
+          {
+            product_id: product.id,
+            size_id: 3
+          }
+        ]),
+
+        db.insert(product_quantity).values([
+          {
+            product_id: product.id,
+            quantity: rand(1, 100)
+          }
+        ]),
+
+        db.insert(product_delivery).values({
+          product_id: product.id,
+          shipping_fee: rand(1000, 3000),
+          duration_days: rand(1, 5)
+        })
+      ]);
+    }
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
+    console.log('Seed data inserted');
+    process.exit(0); // 프로세스 강제 종료
+  }
 })();
