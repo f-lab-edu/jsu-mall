c358fd8c98574eb7cd33e3a28253ea5c8be826f1
감자 <wjdwjdtn92@naver.com>
2024-05-15 09:07:44 +0900

    Feat: Add database env error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "drizzle-kit";
 
 dotenv.config({ path: '.env.local' });
 
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not found in environment');
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./drizzle/schema.ts",
